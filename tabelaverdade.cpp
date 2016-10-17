659dd9c649983643590e69817cbe9d653f634ec1
Valgueiro <mvt@cin.ufpe.br>
2016-10-17 13:03:07 -0300

    findSub funcionando

--- tabelaverdade.cpp
+++ tabelaverdade.cpp
@@ -15,6 +15,13 @@ int atomic(char a){
    if(a == 'x' || a == 'y' || a == 'z' || a == 't') return 1;
    return 0;
 }
+
+void pass(int begin, int end){
+   int i, j;
+   for(i = begin, j=0; i<=end; i++, j++) substr[found][j] = str[i];
+   substr[found][j] = '\0';
+   found++;
+}
 //==================================Fuções das operações======================================
 int Fand(int a, int b){
    if(a==0 || b==0) return 0;
@@ -38,18 +45,29 @@ int Fnot(int a){
 //===========================================================================================
 
 int findSub(vi mkd, int where){
-   
+   int i;
+   for(;str[where]!=')' && mkd[where]!=1 && where<strlen(str); where++);
+   for(i=where; i>=0; i--)  if(str[i]=='(' && mkd[i]==0) break;
+   if(i==-1) return 0;
+
+   pass(i, where);
+   mkd[where] = 1;
+   mkd[i] = 1;
+
+   if(where<strlen(str)) findSub(mkd, where+1);
+
 }
 
 int main(){
+   int i;
    scanf(" %s", str);
-   mkd.resize(strlen(str));
-   for(int i =0; i<strlen(str); i++){
-      mkd[i] = 0;
+
+   for(i = 0; i<strlen(str); i++){
+      mkd.push_back(0);
    }
    found = 0;
    findSub(mkd, 0);
-   for(int i=0; i<found; i++){
+   for(i=0; i<found; i++){
       printf("%s\n",substr[i]);
    }
 
