58294048bea04ba93b894dcd4489ea26179cc471
Valgueiro <mvt@cin.ufpe.br>
2016-10-24 14:03:19 -0300

    formatacao de saida quase pronta

--- tabelaverdade.cpp
+++ tabelaverdade.cpp
@@ -14,11 +14,14 @@ typedef vector <int> vi;
 typedef pair <int, int> ii;
 typedef vector<ii> vii;
 
-int found, atomics;
+int found, atomics, tam;
 int variables[5], value[16][100];
 vi mkd;
 char str[201];//Guarda a completa
 char substr[50][201];//Guarda todas as subexpressões
+
+
+int setvalue(char str[], int linha, int begin, int end);
 
 //=====================================Funções úteis=========================================
 int isAtomic(char a){
@@ -35,22 +38,36 @@ int isAtomic(char a){
 
 int valor(char a, int j){
     int aux = isAtomic(a);
-    if(aux){
-        return value[aux][j];
+    return value[j][aux];
+}
+
+void printLinha(){
+    int i;
+    for(i=0; i<tam; i++){
+        printf("-");
     }
-    return aux;
+    printf("\n");
 }
 
 void print(){
-    int i, j;
+    bool first=true;
+    int i, j, espacos[200];
     //Primeira linha com as subexpressões:
+    printLinha();
     for(i=1; i<found; i++){
         if(i<=4){
             if(variables[i]){
-             printf("%s ", substr[i]);
+                if(first){
+                    printf("|%s|", substr[i]);
+                    first = false;
+                }else{
+                    printf("%s|", substr[i]);
+                }
             }
+            espacos[i]=0;
         }else{
-            printf("%s ",substr[i]);
+            printf("%s|",substr[i]);
+            espacos[i]=strlen(substr[i])-1;
         }
 
     }
@@ -58,14 +75,67 @@ void print(){
 
     //Valores da tabela;
     for(j=0; j<pow(2, atomics); j++){
+        printLinha();
+        first = true;
         for(i=1; i<found; i++){
             if(i<=4){
               if(!variables[i])
                   continue;
             }
-            printf("%d ", value[j][i]);
+            if(first){
+                 printf("|%d|", value[j][i]);
+                 first = false;
+            }else{
+                for(int k=0; k<espacos[i]; k++){
+                    printf(" ");
+                }
+                printf("%d|", value[j][i]);
+            }
         }
         printf("\n");
+    }
+    printLinha();
+}
+
+int final(char str[], int begin){
+    int end = begin;
+    while(str[end]!=')'){
+        end++;
+    }
+    return end-1;
+}
+
+int comeco(char str[], int end){
+    int begin = end;
+    while(str[begin]!='('){
+        begin--;
+    }
+    return begin-1;
+}
+
+int takevalue(int i, char str[], int* aux1, int* aux2, int linha){
+    int ini, fim;
+    if(isAtomic(str[i-1])){
+        *aux1 = valor(str[i-1], linha);
+    }else{
+        ini = comeco(str, i-1);
+        *aux1 = setvalue(str, linha, ini, i-1);
+        printf("sub:");
+        for(int j=ini; j<i-1;j++){
+            printf("%c", str[j]);
+        }
+        printf(" -> value:%d\n", *aux1);
+    }
+    if(isAtomic(str[i+1])){
+        *aux2 = valor(str[i-1], linha);
+    }else{
+        fim = final(str, i+1);
+        *aux2 = setvalue(str, linha, i+1, fim);
+        printf("sub:");
+        for(int j=i+1; j<fim;j++){
+            printf("%c", str[j]);
+        }
+        printf(" -> value:%d\n", *aux2);
     }
 }
 //===========================================================================================
@@ -153,13 +223,57 @@ void init(){
 
     found = 5;
     findSub(0);
+
+    //qtd de ---
+    tam = atomics, i;
+    for(i=5; i<found; i++){
+        tam+=strlen(substr[i]);
+    }
+    tam+=found-1;
 }
 //===========================================================================================
 
+int setvalue(char str[], int linha, int begin, int end){
+    int i, aux, aux1, aux2, fim, ini;
+    for(i=begin; i<end; i++){
+         if(str[i] == '+'){//or
+            printf("or:\n");
+            takevalue(i, str, &aux1, &aux2, linha);
+            return For(aux1, aux2);
+        }else if(str[i] == '.'){
+            printf("and:\n");
+            takevalue(i, str, &aux1, &aux2, linha);
+            return Fand(aux1, aux2);
+        }else if(str[i] == '>'){
+            printf("imp:\n");
+            takevalue(i, str, &aux1, &aux2, linha);
+            return Fimp(aux1, aux2);
+        }else if(str[i] == '-'){//not
+            if(isAtomic(str[i+1])){
+                printf("not:%c\n", str[i+1]);
+                printf("%d->%d\n",valor(str[i+1], linha), Fnot(valor(str[i+1], linha)));
+                return Fnot(valor(str[i+1], linha));
+            }else{
+                fim = final(str, i+1);
+                aux = setvalue(str, linha, i+1, fim);
+                return Fnot(aux);
+            }
+        }
+    }
+}
+
 int main(){
+    int i, j;
     scanf(" %s", str);
 
     init();
+
+    for(i=5; i<found; i++){
+        for(j=0; j<pow(2, atomics); j++){
+            value[j][i] = setvalue(substr[i], j, 0, strlen(substr[i]));
+        }
+    }
+
 
     print();
 
